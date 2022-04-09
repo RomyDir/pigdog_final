67c49b5e44d8eabcd55bcaffdf718ad0e54bf69a
RomyDir <romana.dirnfeld@students.unibe.ch>
2022-04-10 00:53:23 +0200

    new signup, sign in pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
-import 'login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pigdog_final/homePage.dart';
+import 'package:pigdog_final/SignInPage.dart';
+import 'package:pigdog_final/signUpPage.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  bool showProgress = false;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase Authentication"),
-      ),
-      body: Center(
-        child: ModalProgressHUD(
-          inAsyncCall:showProgress ,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Registration Page",
-                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                    hintText: "Enter your Email",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)))),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                    hintText: "Enter your Password",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)))),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Material(
-                elevation: 5,
-                color: Colors.lightBlue,
-                borderRadius: BorderRadius.circular(32.0),
-                child: MaterialButton(
-                  onPressed: () {
-//                  Navigator.push(
-//                    context,
-//                    MaterialPageRoute(builder: (context) => MyLoginPage()),
-//                  );
-                  },
-                  minWidth: 200.0,
-                  height: 45.0,
-                  child: Text(
-                    "Register",
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      home: HomePage(),
+      routes: <String, WidgetBuilder>{
+        "/SignInPage": (BuildContext context) => SignInPage(),
+        "/SignUpPage": (BuildContext context) => SignUpPage(),
+      },
     );
   }
 }
